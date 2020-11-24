d37b783c63ec677638a50dc73894e1327376e146
Janescobar92 <jancore92@gmail.com>
2020-11-24 11:07:59 +0000

    modified models

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Float, Date, Boolean, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Float, Datetime, Boolean, Text
 from datetime import datetime
 
 db = SQLAlchemy()
@@ -7,15 +7,15 @@ db = SQLAlchemy()
 
 class Operations(db.Model):
     __tablename__= "operations"
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    service_id = Column(Integer, ForeignKey("services.id"), primary_key=True)
-    service = Column(String(255), nullable=False)
-    date = Column(String(255), nullable=False)
-    price = Column(Float(), nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    user_id_who_hire = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    service_id_hired = Column(Integer, ForeignKey("services.id"), primary_key=True)
+    date = Column(Datetime(), nullable=False)
+    hired_time = Column(Integer, nullable=False)
+    total_price= Column(Float(), nullable= False)
     # realtionships
-    user_operation = db.relationship("User", back_populates="services_operations")
-    service_operation = db.relationship("Services", back_populates="users_operations")
+    user_operation = db.relationship("User", back_populates="services_operation")
+    service_operations = db.relationship("Services", back_populates="users_operations")
 
 
 class User(db.Model):
@@ -32,8 +32,7 @@ class User(db.Model):
     image = Column(Text())
     # realtionships
     animals = db.relationship('Animals', lazy=True)
-    commerce = db.relationship("Commerce", lazy=True)
-    services_operations = db.relationship("Operations", back_populates="user_operation")
+    services_operation = db.relationship("Operations", back_populates="user_operation")
 
     # def __repr__(self):
     #     return '<User %r>' % self.username
@@ -42,10 +41,15 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "last_name": self.last_name,
+            "phone":self.phone,
+            "location":self.location,
+            "biografy":self.biografy,
             # do not serialize the password, its a security breach
         }
 
-    def create(self):
+    def create_user(self):
         db.session.add(self)
         db.session.commit()
 
@@ -89,24 +93,6 @@ class Animals(db.Model):
 
     # def delete_user():
 
-class Commerce(db.Model):
-    __tablename__="commerce"
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    description = db.Column(db.Text(), nullable=False)
-    url = db.Column(db.String(255))
-    image = db.Column(db.Text())
-    schedule = db.Column(db.String(255), nullable=False)
-    address = db.Column(db.Text(), nullable=False)
-    def create_user(self):
-        db.session.add(self)
-        db.session.commit()
-    # def read_user():
-
-    # def update_user():
-
-    # def delete_user():
-
 class Review(db.Model):
     __tablename__= "review"
     id = db.Column(db.Integer, primary_key=True)
@@ -114,7 +100,7 @@ class Review(db.Model):
     points = db.Column(db.Float())
     text = db.Column(db.Text(), nullable=False)
     # date = db.Column(db.DateTime, nullable=False)
-    review_type = Column(Enum("comercio","usuario"), nullable=False)
+    review_type = Column(Enum("commerce_review","user_review"), nullable=False)
     
     __mapper_args__ = {
         'polymorphic_identity': 'review',
@@ -138,24 +124,16 @@ class User_review(Review):
         'polymorphic_identity': 'user_review'
     }
 
-class Commerce_review(Review):
-    __tablename__= "Commerce_review"
-    id_review = db.Column(db.Integer, ForeignKey("review.id"), primary_key=True)
-    id_commerce_destination = Column(Integer, ForeignKey('commerce.id'), primary_key=True)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'commerce_review'
-    }
 
 class Services(db.Model):
     __tablename__= "services"
     id = Column(Integer, primary_key=True)
-    service = Column(Enum("Paseador", "Canguro", "Adiestrador", "Aseo"))
+    id_service_type = Column(Integer, ForeignKey("service_type.id"))
+    id_user_offer = Column(Integer, ForeignKey("user.id"))
     description = Column(Text(), nullable=False)
-    price = Column(Float(),nullable=False)
-    image = Column(Text())
+    price_h = Column(Float(),nullable=False)
     # realtionships
-    users_operations = db.relationship("Operations", back_populates="service_operation")
+    users_operations = db.relationship("Operations", back_populates="service_operations")
 
     def create_user(self):
         db.session.add(self)
@@ -166,5 +144,9 @@ class Services(db.Model):
 
     # def delete_user():
 
+class Service_type(db.Model):
+    __tablename__="service_type"
+    id = Column(Integer, primary_key=True)
+    service_type = Column(String(255))
 
 
