3d36069780b944967729ef405ea5bfa4d1a54222
Janescobar92 <jancore92@gmail.com>
2020-11-26 22:02:40 +0000

    creating the correct dispousal for the create user endpoint

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+import jwt
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from utils import APIException, generate_sitemap
 from admin import setup_admin
@@ -30,12 +33,16 @@ app.cli.add_command(init_db)
 @app.route('/register', methods=['POST'])
 def create_user():
     body=request.get_json()
-    try:
-        new_user= User(email=body["email"], password=body["password"], is_active=body["is_active"], name=body["name"], last_name=body["last_name"], phone=body["phone"], location=body["location"], biografy=body["biografy"])
-        new_user.create_user()
-        return jsonify(new_user.serialize()), 200
-    except:
-        return "Couldn't create the user",409
+    # try:
+    hashed_password = generate_password_hash(body['password'], method='sha256')
+    print(hashed_password,"estoy en hashedpassword register")
+    new_user= User( email=body["email"], password=hashed_password, is_active=True, name=body["name"], last_name=body["last_name"])
+    #, phone=body["phone"], location=body["location"], biografy=body["biografy"]
+    print(new_user,"soy new user")
+    new_user.create_user()
+    return jsonify(new_user.serialize()), 200
+    # except:
+        # return "Couldn't create the user",409
 
 @app.route('/user/<int:id_user>/pet', methods=['GET'])
 def read_pets_by_user(id_user):
