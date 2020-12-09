e429153a5d64b73e366fa0f98c172594d0425e10
Janescobar92 <jancore92@gmail.com>
2020-12-09 18:56:44 +0000

    adding commented try excepts

--- src/main.py
+++ src/main.py
@@ -41,10 +41,7 @@ def login_user():
     if user.is_active == True:
         if "x-access-tokens" not in request.headers:
             if not body or not body["email"] or not body["password"]:  
-                return 'could not verify', 402  
-
-            # user = User.query.filter_by(email=body["email"]).first()  
-                
+                return 'could not verify', 402    
             if check_password_hash(user.password, body["password"]):  
                 token = jwt.encode({'id': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=500)}, app.config['SECRET_KEY'])  
                 return jsonify({'token' : token.decode('UTF-8')}) 
@@ -65,7 +62,6 @@ def create_user():
     if exists == True :
         user = User.query.filter_by(email=body["email"]).first()
         is_active = user.is_active
-        print(user)
         if is_active == False: 
             hashed_password = generate_password_hash(body['password'], method='sha256')
             old_new_user = user
@@ -104,18 +100,17 @@ def update_loged_user(id_user):
 @app.route('/user/<int:id_user>', methods=['DELETE'])
 def delete_user(id_user):
 
-    # try:
-    user_to_delete = User.delete_user(id_user)
-    return jsonify(user_to_delete.serialize()), 200
-    # except:
-    #     return "Couldn't delete user profile", 401
+    try:
+        user_to_delete = User.delete_user(id_user)
+        return jsonify(user_to_delete.serialize()), 200
+    except:
+        return "Couldn't delete user profile", 401
 
 #///////////////////////////////////////////////////////
 @app.route('/<int:id_service_type>/services', methods=['GET'])
 def get_all_services(id_service_type):  
     try:
         all_services = Services.read_all_services(id_service_type) 
-        print(all_services, "estoy en main /services")
         return jsonify(all_services), 200
     except:
         return "Couldn't find the services",404
@@ -140,7 +135,6 @@ def read_user_services_disabled(id_user):
 # @token_required
 def create_user_service(id_user):
     body=request.get_json()
-    print(body,"estoy en post service")
     try:
         new_service = Services(id_service_type=body["id_service_type"], id_user_offer=id_user, description=body["description"], price_h=body["price_h"])
         new_service.create_service()
@@ -152,27 +146,22 @@ def create_user_service(id_user):
 # @token_required
 def update_user_service(id_user):
     body=request.get_json()
-    print(body,"estoy en put service")
-    
-    update_service= Services(id_service_type=body["id_service_type"], id_user_offer=id_user, description=body["description"], price_h=body["price_h"])
-    print(update_service,"estoy en updateservice 1aaaaaaa")
-    update_service.update_services(body["id_service_type"], id_user, body["description"], body["price_h"])
-    print(update_service,"estoy en updateservice bbbbbbb")
-    return jsonify(update_service.serialize()), 200
-    # except:
-    #     return "Couldn't update the service",404
+
+    try:
+        update_service= Services(id_service_type=body["id_service_type"], id_user_offer=id_user, description=body["description"], price_h=body["price_h"])
+        update_service.update_services(body["id_service_type"], id_user, body["description"], body["price_h"])
+        return jsonify(update_service.serialize()), 200
+    except:
+        return "Couldn't update the service",404
 
 @app.route('/user/<int:id_user>/<int:id_service_type>', methods=['DELETE'])
 # @token_required
 def delete_user_service(id_user, id_service_type):
-    # try:
-    print(id_service_type,"en main deleted")
-   
-    deleted_service = Services.delete_service(id_user,id_service_type)
-    print(deleted_service, "en main deleeted")
-    return jsonify(deleted_service.serialize()), 202
-    # except:
-    #     return "Couldn't delete the service", 409
+    try:
+        deleted_service = Services.delete_service(id_user,id_service_type)
+        return jsonify(deleted_service.serialize()), 202
+    except:
+        return "Couldn't delete the service", 409
 
 
 
@@ -192,12 +181,12 @@ def read_pets_by_user(id_user):
 # @token_required
 def create_user_pet(id_user):
     body=request.get_json()
-    # try:
-    new_user_pet = Animals(user_id = id_user, name = body["name"], image = body["image"], animal_type = body["animal_type"], age = body["age"], personality = body["personality"],  gender = isTrue(body["gender"]) , weight= body["weight"], size = body["size"], diseases= body["diseases"], sterilized= isTrue(body["sterilized"]))
-    new_user_pet.create_user_pet()
+    try:
+        new_user_pet = Animals(user_id = id_user, name = body["name"], image = body["image"], animal_type = body["animal_type"], age = body["age"], personality = body["personality"],  gender = isTrue(body["gender"]) , weight= body["weight"], size = body["size"], diseases= body["diseases"], sterilized= isTrue(body["sterilized"]))
+        new_user_pet.create_user_pet()
     return jsonify(new_user_pet.serialize()), 200
-    # except:
-    #     return "Couldn't create the pet",404
+    except:
+        return "Couldn't create the pet",404
 
 @app.route('/user/<int:id_user>/pet', methods=['PUT'])
 # @token_required
@@ -237,20 +226,19 @@ def read_pets_character():
 
 @app.route('/user/workedfor/<int:id_user_param>', methods=['GET'])
 def read_history_workedfor(id_user_param):
-    print(id_user_param)
-    # try:
-    history_service = Services.read_service_ofered(id_user_param)
-    return jsonify(history_service), 200
-    # except:
-    #     return "Couldn't find  history", 409
+     try:
+        history_service = Services.read_service_ofered(id_user_param)
+        return jsonify(history_service), 200
+     except:
+        return "Couldn't find  history", 409
 
 @app.route('/user/hired/<int:id_user>', methods=['GET'])
 def read_history_hired(id_user):
-    # try:
-    history_service_hired = Operations.read_service_hired(id_user)
-    return jsonify(history_service_hired), 200
-    # except:
-    #     return "Couldn't find  history", 409
+    try:
+        history_service_hired = Operations.read_service_hired(id_user)
+        return jsonify(history_service_hired), 200
+    except:
+        return "Couldn't find  history", 409
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
